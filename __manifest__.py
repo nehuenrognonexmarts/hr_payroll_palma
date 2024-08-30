074500c36718f9d750489854c9098d671c458e3a
nehuen-xmarts <nehuen.rognone@xmarts.com>
2024-08-30 03:49:14 -0300

    version cambiada manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Reglas de Palmas San Jose',
-    'version': '17.0.0.0.0',
+    'version': '16.0.0.0.0',
     # 'category': 'none',
     'author': 'Xmarts',
     'website': 'https://wwww.xmarts.com/',
