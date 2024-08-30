92947e4caf6014be196c2d728cd19df76653ba1d
nehuen-xmarts <nehuen.rognone@xmarts.com>
2024-08-30 05:11:01 -0300

    asuestos beta

--- models/hr_salary_rule.py
+++ models/hr_salary_rule.py
@@ -1,5 +1,5 @@
 from odoo import models, fields
-from datetime import datetime
+from datetime import timedelta
 
 
 class HrPaylip(models.Model):
@@ -37,4 +37,32 @@ class HrPaylip(models.Model):
             self.septimo = sum(xma_task_ids.mapped('total'))/len(set(days))
 
     def _compute_asuetos(self):
-        self.asuetos = 0
+        cant_asuetos = 0
+        resource_calendar_leaves = self.env['resource.calendar.leaves']
+        
+        calendar_leaves = resource_calendar_leaves.search([
+            ('date_to', '>=', self.date_from),
+            ('date_from', '<=', self.date_to),
+        ])
+
+        for leave in calendar_leaves:
+            inicio_festivo = max(self.date_from, leave.date_from.date())
+            final_festivo = min(self.date_to, leave.date_to.date())
+
+            if inicio_festivo <= final_festivo:
+                dias_festivos = (final_festivo - inicio_festivo).days + 1
+                cant_asuetos += dias_festivos
+        
+        if cant_asuetos == 0:
+            self.asuetos = 0
+        else:
+            inicio_semana = self.date_from - timedelta(days=7)
+            final_semana = self.date_to - timedelta(days=7)
+            xma_task = self.env['xma.task.activity']
+            xma_task_ids = xma_task.search([
+                ('date', '>=', inicio_semana),
+                ('date', '<=', final_semana),
+                ('employee_id', '=', self.employee_id.id)
+            ])
+            valor_tarea_pasado = sum(xma_task_ids.mapped('total'))
+            self.asuetos = cant_asuetos * (valor_tarea_pasado/6)
